1a968b540cd2da2effd5feb2d75f9c5edd9da677
froman1 <froman@stratechcorp.com>
2021-08-12 18:59:49 -0400

    fix progressbar finish

--- pages/_app.js
+++ pages/_app.js
@@ -5,15 +5,15 @@ import Router from "next/router";
 import ProgressBar from "@badrap/bar-of-progress";
 
 const progress = new ProgressBar({
-  size: 8,
-  color: "#FE595E",
-  className: "z-50",
-  delay: 100,
+  size: 4,
+  color: '#fe595e',
+  className: 'z-50',
+  delay: 100
 });
 
-Router.events.on("routeChangeStart", progress.start);
-Router.events.on("routerChangeComplete", progress.finish);
-Router.events.on("routerChangeError", progress.finish);
+Router.events.on("routeChangeStart", progress.start)
+Router.events.on("routeChangeComplete", progress.finish)
+Router.events.on("routeChangeError", progress.finish)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
